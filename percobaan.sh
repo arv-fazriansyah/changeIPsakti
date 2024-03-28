7c72301d26390e62f3929dd9295964f3ccd39ece
Arvib Fazriansyah <85182275+arv-fazriansyah@users.noreply.github.com>
2024-03-28 23:48:35 +0700

    Update percobaan.sh

--- percobaan.sh
+++ percobaan.sh
@@ -1,47 +1,23 @@
 #!/bin/bash
 
-# Fungsi untuk mengaktifkan mode pesawat secara manual
-activate_airplane_mode_manually() {
-    echo "Silakan aktifkan mode pesawat secara manual."
-}
+# Mendapatkan IP dari output ip route
+ip_address=$(ip route | awk '/src/ {print $9}')
+echo "IP Address: $ip_address"
 
-# Fungsi untuk memeriksa IP sampai mencapai 100
-check_ip_until_100() {
-    echo "Menunggu hingga IP mencapai 100..."
-    while :
-    do
-        ip_address=$(ip route | awk '/src/ {print $9}')
-        ip_part_a=$(echo $ip_address | awk -F'.' '{print $1}')
+# Memisahkan bagian "A" dari alamat IP
+ip_part_a=$(echo $ip_address | awk -F'.' '{print $1}')
 
-        if [ $ip_part_a -ge 100 ]; then
-            echo "IP mencapai 100. Keluar dari loop."
-            break
-        fi
-        sleep 1
-    done
-}
+# Cek apakah bagian "A" dari IP tidak sama dengan 100 atau lebih
+while [ $ip_part_a -lt 100 ]; do
+    echo "IP Address tidak memenuhi kondisi. Menjalankan mode pesawat..."
+    # Mengaktifkan mode pesawat
+    settings put global airplane_mode_on 1
+    am broadcast -a android.intent.action.AIRPLANE_MODE --ez state true
+    sleep 10  # Tunggu 10 detik sebelum memeriksa IP lagi
+    # Mendapatkan IP lagi
+    ip_address=$(ip route | awk '/src/ {print $9}')
+    echo "IP Address: $ip_address"
+    ip_part_a=$(echo $ip_address | awk -F'.' '{print $1}')
+done
 
-# Periksa apakah pengguna adalah root atau tidak
-if [ $(id -u) -eq 0 ]; then
-    echo "User adalah root. Mode pesawat akan diaktifkan secara otomatis dan dimatikan setelah menemukan IP 100."
-    
-    # Aktifkan mode pesawat secara otomatis
-    echo "Aktifkan mode pesawat..."
-    adb shell settings put global airplane_mode_on 1
-    adb shell am broadcast -a android.intent.action.AIRPLANE_MODE --ez state true
-
-    # Memeriksa IP sampai mencapai 100
-    check_ip_until_100
-
-    # Matikan mode pesawat ketika IP mencapai 100
-    echo "Matikan mode pesawat..."
-    adb shell settings put global airplane_mode_on 0
-    adb shell am broadcast -a android.intent.action.AIRPLANE_MODE --ez state false
-
-else
-    echo "User bukan root. Mode pesawat akan diaktifkan secara manual."
-    activate_airplane_mode_manually
-    
-    # Memeriksa IP sampai mencapai 100
-    check_ip_until_100
-fi
+echo "IP Address memenuhi kondisi."
