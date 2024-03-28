12f899bc06510e4195bae57d8b8df2fe5852d61d
Arvib Fazriansyah <85182275+arv-fazriansyah@users.noreply.github.com>
2024-03-28 23:23:54 +0700

    Update percobaan.sh

--- percobaan.sh
+++ percobaan.sh
@@ -1,18 +1,35 @@
 #!/bin/bash
 
-# Mendapatkan IP dari output ip route
-ip_address=$(ip route | awk '/src/ {print $9}')
-echo "IP Address: $ip_address"
-
-# Memisahkan bagian "A" dari alamat IP
-ip_part_a=$(echo $ip_address | awk -F'.' '{print $1}')
-
-# Mengecek apakah bagian "A" dari IP tidak sama dengan 100 atau lebih
-if [ $ip_part_a -lt 100 ]; then
-    # Jika tidak memenuhi kondisi, jalankan mode pesawat
-    echo "IP Address tidak memenuhi kondisi. Menjalankan mode pesawat..."
-    # Perintah untuk menjalankan mode pesawat, gantilah dengan perintah yang sesuai di perangkat Anda
+# Periksa apakah pengguna adalah root atau tidak
+if [ $(id -u) -eq 0 ]; then
+    echo "User adalah root. Mode pesawat akan diaktifkan secara otomatis dan dimatikan setelah menemukan IP 100."
+    
+    # Aktifkan mode pesawat secara otomatis
+    # Perintah untuk mengaktifkan mode pesawat, gantilah dengan perintah yang sesuai di perangkat Anda
     # Misal: am start -a android.settings.AIRPLANE_MODE_SETTINGS
+    echo "Aktifkan mode pesawat..."
+    # am start -a android.settings.AIRPLANE_MODE_SETTINGS
+
+    # Looping untuk menunggu hingga IP mencapai 100
+    while :
+    do
+        ip_address=$(ip route | awk '/src/ {print $9}')
+        ip_part_a=$(echo $ip_address | awk -F'.' '{print $1}')
+
+        if [ $ip_part_a -ge 100 ]; then
+            # Matikan mode pesawat ketika IP mencapai 100
+            # Perintah untuk mematikan mode pesawat, gantilah dengan perintah yang sesuai di perangkat Anda
+            # Misal: am start -a android.settings.AIRPLANE_MODE_SETTINGS
+            echo "IP mencapai 100. Matikan mode pesawat..."
+            # am start -a android.settings.AIRPLANE_MODE_SETTINGS
+            break
+        fi
+        sleep 1
+    done
+
 else
-    echo "IP Address memenuhi kondisi."
+    echo "User bukan root. Mode pesawat akan diaktifkan secara manual."
+    
+    # Perintah untuk meminta pengguna mengaktifkan mode pesawat secara manual
+    echo "Silakan aktifkan mode pesawat secara manual."
 fi
